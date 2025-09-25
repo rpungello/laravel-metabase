6505ab15b6aa13dbce4e2cf57523df428b1ae9a6
Rob Pungello <rob@pdfsystems.com>
2025-09-25 11:59:26 -0400

    Add helpers for getTable and getField

--- src/Data/DatabaseMetadata.php
+++ src/Data/DatabaseMetadata.php
@@ -2,18 +2,32 @@
 
 namespace Rpungello\Metabase\Data;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\ItemNotFoundException;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
 
 class DatabaseMetadata extends Data
 {
     /**
-     * @param  Table[]  $tables
+     * @param array $features
+     * @param string $name
+     * @param Collection<Table> $tables
      */
     public function __construct(
         public array $features,
         public string $name,
         #[DataCollectionOf(Table::class)]
-        public array $tables,
+        public Collection $tables,
     ) {}
+
+    /**
+     * @param string $name
+     * @return Table
+     * @throws ItemNotFoundException
+     */
+    public function getTable(string $name): Table
+    {
+        return $this->tables->firstOrFail(fn (Table $table) => $table->name === $name);
+    }
 }
--- src/Data/Table.php
+++ src/Data/Table.php
@@ -2,18 +2,32 @@
 
 namespace Rpungello\Metabase\Data;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\ItemNotFoundException;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
 
 class Table extends Data
 {
     /**
-     * @param  Field[]  $fields
+     * @param string $name
+     * @param string|null $description
+     * @param Collection<Field> $fields
      */
     public function __construct(
         public string $name,
         public ?string $description,
         #[DataCollectionOf(Field::class)]
-        public array $fields,
+        public Collection $fields,
     ) {}
+
+    /**
+     * @param string $name
+     * @return Field
+     * @throws ItemNotFoundException
+     */
+    public function getField(string $name): Field
+    {
+        return $this->fields->firstOrFail(fn (Field $field) => $field->name === $name);
+    }
 }
