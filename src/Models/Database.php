8b84fa418ff0d12cc12926d66a334032313d6715
Rob Pungello <rob@pdfsystems.com>
2025-09-25 14:17:32 -0400

    Add helpers for getting data objects

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -3,6 +3,12 @@
 namespace Rpungello\Metabase\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Collection;
+use Phiki\Transformers\Meta;
+use Rpungello\Metabase\Data\DatabaseMetadata;
+use Rpungello\Metabase\Facades\Metabase;
 
 class Database extends Model
 {
@@ -19,4 +25,31 @@ class Database extends Model
         'database_id' => 'integer',
         'schema_version' => 'integer',
     ];
+
+    /**
+     * @throws RequestException
+     * @throws ConnectionException
+     */
+    public function toData(): Database
+    {
+        return Metabase::getDatabase($this->database_id);
+    }
+
+    /**
+     * @throws RequestException
+     * @throws ConnectionException
+     */
+    public function getMetadata(): DatabaseMetadata
+    {
+        return Metabase::getDatabaseMetadata($this->database_id);
+    }
+
+    /**
+     * @throws RequestException
+     * @throws ConnectionException
+     */
+    public function getMetabaseTables(): Collection
+    {
+        return $this->getMetadata()->tables;
+    }
 }
