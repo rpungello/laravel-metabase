c62b072dbe8a2418e6c7f3daeb774404b0318669
Rob Pungello <rob@rpun.io>
2025-09-25 13:25:23 -0400

    Add defaults to Database.php

--- src/Data/Database.php
+++ src/Data/Database.php
@@ -7,9 +7,9 @@ use Spatie\LaravelData\Data;
 class Database extends Data
 {
     public function __construct(
-        public int $id,
+        public ?int $id,
         public string $name,
         public ?string $description,
-        public array $features,
+        public array $features = [],
     ) {}
 }
